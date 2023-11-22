42540f2aa333e33d290727a3a41169b0e3a2cc12
Seweryn <sewerynaleksdutka@gmail.com>
2023-11-22 17:30:16 +0100

    'preview'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const movies = [
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+
+    // Dodaj więcej filmów według potrzeb
+  ];
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <div className="logo-search">
+            <div className="logo">
+              <img src="logo.png" />
+            </div>
+            <div className="search-bar" alt="Logo">
+              <input type="text" placeholder="Search" />
+            </div>
+          </div>
+          <div id="login">
+            <button className="login-button">Log In</button>
+          </div>
+        </header>
+
+        <div className="container">
+          <div id="search-results" className="movie-tiles">
+            {movies.map((movie, index) => (
+              <div key={index} className="movie-tile">
+                <img src={movie.image} alt={movie.title} />
+                <h3>{movie.title}</h3>
+                <p>{movie.description}</p>
+                <div className="rating">
+                  {Array.from({ length: movie.rating }, (_, i) => (
+                    <span key={i}>&#9733;</span>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <footer className="footer">
+          <div class="copyright">
+            © Baza Filmow - Seweryn Dutka
+          </div>
+        </footer>
+        
+      </div>
+
+    );
+  }
+
+  export default App;
